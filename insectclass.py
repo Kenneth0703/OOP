f7d010ce90c3ef5cc9cad8779e97835cf0b557b7
Kenneth Schoel <Kenneth_schoel1@baylor.edu>
2023-02-10 21:04:32 -0600

    Bug Flight

--- insectclass.py
+++ insectclass.py
@@ -8,8 +8,7 @@ class Bug:
         self.legs = 2
 
     def fly(self):
-        if random.randint(1,11) == 1:
-            self.flight = "Distance"
+        self.flight = random.randint(0,10)
         
     def get_flight(self):
         return self.flight
--- insectfly.py
+++ insectfly.py
@@ -4,7 +4,7 @@ def main():
     my_bug = ins.Bug()
     print("my bug flew")
 
-    for count in range(10):    
+    for count in range(1000):    
         print("my bug flew this far:",my_bug.get_flight())
         my_bug.fly()
 main()
